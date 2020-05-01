8d6a883145b81a017e5b983cf5d3252efaa0283e
programerr01 <nitesh6pandey6@gmail.com>
2020-05-01 13:28:34 +0530

    fixed minor bugs

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ red.oninput = function name(){
 }
 
 
-var blue = document.getElementById("g")
+var blue = document.getElementById("b")
 blue.oninput = function(){
     document.documentElement.style.setProperty('--blue', `${this.value}`);
     document.body.style.background = `rgb(${red.value},${green.value},${blue.value})`;
 }
 
 
-var green = document.getElementById("b")
+var green = document.getElementById("g")
 green.oninput = function(){
     document.documentElement.style.setProperty('--green', `${this.value}`);
     document.body.style.background = `rgb(${red.value},${green.value},${blue.value})`;
 }
 
+function componentToHex(c){
+var hex  = c.toString(16);
+    return hex.length == 1? "0" + hex : hex;}
+function hex1(r,g,b){
+return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);}
 
+function hex(){
+document.getElementById("p").innerText = hex1(Number(red.value) , Number(green.value) , Number(blue.value)) }
 
 
